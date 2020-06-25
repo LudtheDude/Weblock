b7370502925fd63a0f4413f96499937e647b35e6
LudtheDude <59583159+LudtheDude@users.noreply.github.com>
2020-06-25 18:49:22 -0400

    Update textClean.py

--- textClean.py
+++ textClean.py
@@ -1,3 +1,5 @@
+# CLEAN THE TEXT
+import string
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
@@ -8,7 +10,6 @@ tokens = word_tokenize(text)
 
 tokens = [w.lower() for w in tokens]
 
-import string
 table = str.maketrans('', '', string.punctuation)
 stripped = [w.translate(table) for w in tokens]
 
@@ -16,8 +17,15 @@ words = [word for word in stripped if word.isalpha()]
 
 stop_words = set(stopwords.words('english'))
 words = [w for w in words if not w in stop_words]
-print(words[:100])
+
 
 porter = PorterStemmer()
-stemmed = [porter.stem(word) for word in tokens]
-print(stemmed[:100])
+stemmed_text = [porter.stem(word) for word in tokens]
+
+
+
+# SORT THE WORDS BY FREQUENCY
+
+single_text = sorted(set(stemmed_text)) #remove duplicate words and sort
+for x in single_text:
+    freq_list = stemmed_text.count(x), x
