cde5ca21d5df570ab6754959ca401d514254cce6
katezhang1234 <66185466+katezhang1234@users.noreply.github.com>
2020-06-25 13:17:27 -0700

    Update newpageButtons.js

--- newpageButtons.js
+++ newpageButtons.js
@@ -1,4 +1,4 @@
-function addWebsite(){
+/*function addWebsite(){
     //var website = document.getElementById('websites-to-block').value;
     //document.getElementById("websites-to-block").value = '';
     console.log(document.getElementById('websites-to-block').value);
@@ -9,3 +9,20 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('add-website').addEventListener('click', addWebsite);
     main();
   });
+  */
+window.blocked = [];
+
+document.getElementById('add-website').onclick = function(){
+	window.addedSite = document.getElementById("websites-to-block").value
+	window.blocked.push(window.addedSite)
+	chrome.runtime.sendMessage({
+  		added: addedSite
+	})
+	var length = window.blocked.length
+	const div = document.createElement('div')
+	div.textContent = length + ". " + addedSite
+	document.getElementById("printedList").appendChild(div)
+
+	document.getElementById("websites-to-block").value = "";
+	window.addedSite = "";
+}
