c380b5ed32c2191289f28b5445f1786db4edcbc8
katezhang1234 <66185466+katezhang1234@users.noreply.github.com>
2020-06-25 13:16:42 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,3 +1,27 @@
+//BLOCK THE ENTIRE DOMAIN 
+window.blockedUrls = [];
+chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
+  window.blockedUrls.push(request.added)
+})
+BlockAllURL = function changeAllURL(text){
+  var current = window.location.href;
+  for(var i = 0;i<text.length;i++){
+    if(current.includes(text[i])){
+      document.documentElement.innerHTML = 'The page you are trying to access has been blocked';
+    }
+  }
+}
+BlockAllURL(window.blockedUrls);
+
+
+chrome.runtime.sendMessage({
+  url: window.location.href
+})
+
+
+
+
+
 //BLOCK WORDS
 BlockString = function findText(text) {
   if(window.find(text)){
@@ -7,38 +31,37 @@ BlockString = function findText(text) {
   };
 }
 
-//BlockString("WordToBlock");
-
-
-
 //BLOCK THE PARTIAL DOMAINS
 BlockURL = function changeURL(text){
   var current = window.location.href;
   if(current === text){
-    //window.location.replace("https://www.google.co.in");
-    document.documentElement.innerHTML = 'Website is blocked';
+    window.location.replace("https://www.google.co.in");
   }
 }
 
-//BLOCK THE ENTIRE DOMAIN WITH THE FOLLOWING FUNCTION
-BlockAllURL = function changeAllURL(text){
-  var current = window.location.href;
-  if(current.startsWith(text)){
-    //document.documentElement.innerHTML = '';
-    document.documentElement.innerHTML = 'Website is blocked';
-    //document.documentElement.scrollTop = 0;
-  }
-}
-
-BlockAllURL("https://www.facebook.com/");
 
 
-chrome.runtime.sendMessage({
-  url: window.location.href,
-})
 
 
  /*chrome.webRequest.onBeforeRequest.addListener(
         function(details) { return {cancel: true}; },
         {urls: ["*://www.evil.com/*"]},
-        ["blocking"]);*/
+        ["blocking"]);
+  */
+
+
+
+
+
+//BLOCKS ENTIRE WEBSITE
+/*function blockRequest(details) {
+   return {cancel: true};
+}
+
+function updateFilters(urls) {
+   if(chrome.webRequest.onBeforeRequest.hasListener(blockRequest))
+     chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
+   chrome.webRequest.onBeforeRequest.addListener(blockRequest, {urls: ["*://*.facebook.com/*", "*://*.facebook.net/*"]}, ['blocking']);
+}
+
+updateFilters();*/
