5cca63e8e05c226ef1422d812be4debb449dfcfb
LudtheDude <59583159+LudtheDude@users.noreply.github.com>
2020-06-25 20:36:44 -0400

    Update textClean.py

--- textClean.py
+++ textClean.py
@@ -1,3 +1,5 @@
+# ADD BUTTON THAT BEGINS THIS SEQUENCE
+
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 import urllib.request
@@ -5,49 +7,60 @@ import string
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+from googlesearch import search 
+
+while True:
+
+	# EXTRACT TEXT FILE FROM HTML OF WEBSITE
+
+	def tag_visible(element):
+	    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+	        return False
+	    if isinstance(element, Comment):
+	        return False
+	    return True
+
+	def text_from_html(body):
+	    soup = BeautifulSoup(body, 'html.parser')
+	    texts = soup.findAll(text=True)
+	    visible_texts = filter(tag_visible, texts)  
+	    return u" ".join(t.strip() for t in visible_texts)
+
+	html = urllib.request.urlopen(url).read()
+	full_text = text_from_html(html)
 
 
-# EXTRACT TEXT FILE FROM HTML OF WEBSITE
+	# CLEAN UP THE TEXT FILE 
 
-def tag_visible(element):
-    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
-        return False
-    if isinstance(element, Comment):
-        return False
-    return True
+	tokens = word_tokenize(full_text)
 
-def text_from_html(body):
-    soup = BeautifulSoup(body, 'html.parser')
-    texts = soup.findAll(text=True)
-    visible_texts = filter(tag_visible, texts)  
-    return u" ".join(t.strip() for t in visible_texts)
+	tokens = [w.lower() for w in tokens]
 
-html = urllib.request.urlopen(*INSERT TEXT FILE FROM BLOCKED LIST*).read()
-full_text = text_from_html(html)
+	table = str.maketrans('', '', string.punctuation)
+	stripped = [w.translate(table) for w in tokens]
 
+	words = [word for word in stripped if word.isalpha()]
+
+	stop_words = set(stopwords.words('english'))
+	words = [w for w in words if not w in stop_words]
+
+	porter = PorterStemmer()
+	stemmed_text = [porter.stem(word) for word in tokens]
 
 
-# CLEAN UP THE TEXT FILE 
+	# SORT THE WORDS BY FREQUENCY
 
-tokens = word_tokenize(full_text)
-
-tokens = [w.lower() for w in tokens]
-
-table = str.maketrans('', '', string.punctuation)
-stripped = [w.translate(table) for w in tokens]
-
-words = [word for word in stripped if word.isalpha()]
-
-stop_words = set(stopwords.words('english'))
-words = [w for w in words if not w in stop_words]
-
-porter = PorterStemmer()
-stemmed_text = [porter.stem(word) for word in tokens]
+	single_text = sorted(set(stemmed_text))
+	for x in single_text:
+	    freq_list = stemmed_text.count(x), x
 
 
+	# SEARCH THE WEB FOR NEW WEBSITES USING FREQUENT KEYWORDS
+	
+	i = 0
 
-# SORT THE WORDS BY FREQUENCY
-
-single_text = sorted(set(stemmed_text))
-for x in single_text:
-    freq_list = stemmed_text.count(x), x
+	while i < 3:
+		query = freq_list[i]
+		for url in search(query, tld="co.in", num=1, stop=1, pause=2): 
+	    	print(url)
+	    i = i + 1
