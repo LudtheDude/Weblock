afa9f8047324186d3059be5e55340a85bea3a0f0
LudtheDude <59583159+LudtheDude@users.noreply.github.com>
2020-06-25 20:00:51 -0400

    Update textClean.py

--- textClean.py
+++ textClean.py
@@ -1,12 +1,32 @@
 # CLEAN THE TEXT
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+import urllib.request
 import string
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)  
+    return u" ".join(t.strip() for t in visible_texts)
+
+html = urllib.request.urlopen(*INSERT TEXT FILE FROM BLOCKED LIST*).read()
+full_text = text_from_html(html)
+
 # need to find a way to get a website's 'text'
 
-tokens = word_tokenize(text)
+tokens = word_tokenize(full_text)
 
 tokens = [w.lower() for w in tokens]
 
@@ -26,6 +46,6 @@ stemmed_text = [porter.stem(word) for word in tokens]
 
 # SORT THE WORDS BY FREQUENCY
 
-single_text = sorted(set(stemmed_text)) #remove duplicate words and sort
+single_text = sorted(set(stemmed_text))
 for x in single_text:
     freq_list = stemmed_text.count(x), x
