26ba90064fe4b3318b59a4a892042627a494e749
katezhang1234 <66185466+katezhang1234@users.noreply.github.com>
2020-06-26 05:50:48 -0700

    Delete newPage.js

--- newPage.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded',function(){
-	const background = chrome.extension.getBackgroundPage()
-	for(var i = 0;i<background.urls.length;i++){
-		const div = document.createElement('div')
-		div.textContent = background.urls[i]
-		document.body.appendChild(div)
-	}
-},false)
